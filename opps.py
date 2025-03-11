82a8c004ffbb5b0c7b85f1a5fd8a015c93ae7a89
rahul1387 <rahultripathi7447@gmail.com>
2025-03-11 21:40:46 +0530

    opps

--- opps.py
+++ opps.py
@@ -23,3 +23,22 @@ dog.speak()
 animal  = Animal("Dog")
 animal.speak()  
 
+# multi inheritance
+
+class Parent1:
+    def method1(self):
+        print(f'Method 1 from parent 1')
+
+class Parent2(Parent1):
+    def method2(self):
+        print('Method 2 from parent 2')
+
+class Parent3(Parent2):
+    def method3(self):
+        print('Method 3 from parent 3')
+
+
+Parent3= Parent3()
+Parent3.method1()
+Parent3.method2()
+Parent3.method3()
